af4d9528a5918c3185dab936c3664e105b4ce4b5
Yu Wang <yuw@nvidia.com>
2021-10-28 15:58:27 -0700

    [MakeNet] Enable TF2 pruning

---
+++ pruning/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
+"""Modulus pruning APIs."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from pruning import pruning
+from pruning.pruning import prune
+
+__all__ = ("prune", "pruning")
