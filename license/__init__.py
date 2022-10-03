db8aea465b568ba84f0c416db94ae59afc7038d4
Ramanathan Arunachalam <rarunachalam@nvidia.com>
2022-10-03 15:14:15 -0700

    [LICENSE] Fix typo

--- license/__init__.py
+++ license/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2017-2022, NVIDIA CORPORATION.  All rights reserved.
 
-"""EULA for TAO PYTORCH."""
+"""EULA for TAO TF2."""
 
 from __future__ import absolute_import
 from __future__ import division
