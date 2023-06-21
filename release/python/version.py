a6599e25b58ee22976bb529167d50e9b50edf5ec
Varun Praveen <vpraveen@nvidia.com>
2023-06-21 11:53:22 -0700

    [Common] Add apache2.0 license header

--- release/python/version.py
+++ release/python/version.py
@@ -1,4 +1,16 @@
-# Copyright (c) 2022-2023, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """Version string for the TAO Toolkit TF2 AI models/tasks."""
 
@@ -45,5 +57,5 @@ __keywords__ = "nvidia, tao, tf2"
 __contact_names__ = "Yu Wang"
 __contact_emails__ = "yuw@nvidia.com"
 
-__license__ = "NVIDIA Proprietary Software"
+__license__ = "Apache 2.0"
 
